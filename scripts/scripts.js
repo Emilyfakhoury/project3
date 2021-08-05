25b3787548f831098e5a06bdde7e516c9a43ce68
Emily Fakhoury <efakhoury@ufl.edu>
2021-08-05 18:49:18 -0400

    lightbox and slick set up

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slideshow').slick({
+    setting-name: setting-value
+  });
+});
+
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
